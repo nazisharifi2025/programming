961fd2243f5fc46fe26b253748a638af8f963a99
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-13 20:00:59 +0430

    l

---
+++ inputValue/input.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("Savebtn");
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const input3 = document.getElementById("input3");
+const shoing = document.getElementById("shoing");
