c6954b7f71246e9852226006ec30c0dd16b6bdc8
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-14 23:40:03 +0430

    l

---
+++ inputValue/teacher.js
@@ -0,0 +1,7 @@
+const divshoing = document.getElementById("divshoing");
+const input = document.getElementById("input");
+const button = document.getElementById("butten");
+const listofmovis = ["زخم کاری", "تمساح خونی", "شهرزاد", "دل", "پدر"];
+button.addEventListener("click", () => {
+  listofmovis.filter();
+});
