cc182bd207a65fd58b5237e0e0c22defe27ddb1c
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-18 22:20:05 +0430

    l

--- t.js
+++ t.js
@@ -1,6 +1,6 @@
 const body = document.body;
 const div = document.createElement("div");
-// const h1 = document.createElement("h1");
+const h1 = document.createElement("h1");
 // h1.textContent = "in the name of allah";
 // div.style.background = "red";
 // div.classList.add("text-white");
